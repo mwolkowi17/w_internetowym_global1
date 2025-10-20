637d549bfe1be8ce4616224cb42880526a740425
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2025-10-20 21:01:33 +0200

    setting up new concept

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { ref, onMounted, onUnmounted } from 'vue'
+import MainPage from './views/MainPage.vue';
+
+const scale = ref(1)
+
+function updateScale() {
+    const widthRatio = window.innerWidth / 1920
+    const heightRatio = window.innerHeight / 1080
+    scale.value = Math.min(widthRatio, heightRatio)
+}
+
+onMounted(() => {
+    updateScale()
+    window.addEventListener('resize', updateScale)
+})
+
+onUnmounted(() => {
+    window.removeEventListener('resize', updateScale)
+})
+
+</script>
+
+<template>
+    <div :style="{ transform: `scale(${scale})`, transformOrigin: 'top left' }" role="application">
+        <MainPage />
+    </div>
+</template>
+
+<style scoped></style>
---
+++ src/views/MainPage.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import StartView from './StartView.vue';
+
+</script>
+
+<template>
+<StartView/>
+</template>
+
+<style scoped>
+</style>
